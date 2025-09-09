3e134e812083ea161ccbd3bf38b55b20bf2c25c4
Aubakerzzz <abubakerajazz@gmail.com>
2025-09-09 15:07:30 +0500

    Style Issue Fixed

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 
-app =   Flask(__name__)
+app = Flask(__name__)
 app.secret_key = "your_secret_key"  # Required for sessions & flash messages
 
 # Hardcoded credentials for demo
